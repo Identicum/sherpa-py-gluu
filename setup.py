14c97e398c6dc45b5bba7e0e9a48c60b281be8c9
Gustavo J Gallardo <ggallard@identicum.com>
2024-10-25 22:48:35 -0300

    initial commit, migrated from RS

---
+++ setup.py
@@ -0,0 +1,22 @@
+# sherpa-py-gluu is available under the MIT License. https://github.com/Identicum/sherpa-py-gluu/
+# Copyright (c) 2024, Identicum - https://identicum.com/
+#
+# Author: Gustavo J Gallardo - ggallard@identicum.com
+#
+
+from setuptools import setup
+
+setup(
+    name='sherpa-py-gluu',
+    version='1.0.20241025',
+    description='Python utilities for Gluu',
+    url='git@github.com:Identicum/sherpa-py-gluu.git',
+    author='Identicum',
+    author_email='ggallard@identicum.com',
+    license='MIT License',
+    install_requires=['python-ldap', 'ldif', 'pyDes', 'sherpa-py-utils'],
+    packages=['sherpa.gluu'],
+    zip_safe=False,
+    python_requires='>=3.0'
+)
+
