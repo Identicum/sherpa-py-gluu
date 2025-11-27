da57ae2ef8b9c084e334f4fd14cfdfaf86dc80c1
sandovalezequiel <sandovalezequiel@gmail.com>
2025-11-27 19:22:34 +0200

    automated manual migration steps for providers

--- sherpa/gluu/gluu_lib.py
+++ sherpa/gluu/gluu_lib.py
@@ -935,10 +935,10 @@ class GluuBackup:
             else:
                 entity_id = entity.get(file_attr_name)
                 if entity_id not in (set(transformer.default_objs()) - set(include_default)):
-                    json_data = transformer.transform()
-                    file_identifier = json_data["inum"] if entity_type == "client41to45" else entity_id
+                    t = transformer.transform()
+                    file_identifier = t.data.get("inum") if entity_type == "client41to45" else entity_id
                     file_name = f"{file_identifier}.json"
-                    json_data.save_to_file(file_name)
+                    t.save_to_file(file_name)
 
         self.logger.debug(f"{len(entities)} items backed up to {self.output_path or 'backup'}.")
 
